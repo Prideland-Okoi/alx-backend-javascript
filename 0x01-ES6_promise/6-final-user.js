700c9b90414568c1a33af27ee3e55a2d15fbed1d
Prideland-Okoi <prideland.okoi@gmail.com>
2023-01-27 15:07:05 +0000

    included array for profile

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,21 +3,21 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-	const signup = signUpUser(firstName, lastName);
-	const upload = uploadPhoto(fileName);
+  const signup = signUpUser(firstName, lastName);
+  const upload = uploadPhoto(fileName);
 
-	return Promise.allSettled([signup, upload]).then((vals) =>{
-		const resArray = [];
-		vals.forEach((val) => {
-			if (val.status === 'fulfilled') {
-				resArray.push({status: val.status, value: val.value});
-			}else{
-				resArray.push({
-					status: val.status,
-					value: `Error: ${val.reason.message}`,
-				});
-			}
-		});
-		return resArray;
-	});
+  return Promise.allSettled([signup, upload]).then((vals) => {
+    const resArray = [];
+    vals.forEach((val) => {
+      if (val.status === 'fulfilled') {
+        resArray.push({ status: val.status, value: val.value });
+      } else {
+        resArray.push({
+          status: val.status,
+          value: `Error: ${val.reason.message}`,
+        });
+      }
+    });
+    return resArray;
+  });
 }
