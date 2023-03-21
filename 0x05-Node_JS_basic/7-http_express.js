d77924b26c63964c01b268851bdf494ea58fb442
Prideland-Okoi <prideland.okoi@gmail.com>
2023-03-21 14:18:20 +0000

    file updated

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -9,17 +9,22 @@ const app = express();
 const port = 1245;
 
 app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/plain');
+	res.send('Hello Holberton School!');
 });
 
 app.get('/students', async (req, res) => {
-  const msg = 'This is the list of our students\n';
-  try {
-    const students = await countStudents(DATABASE);
-    res.send(`${msg}${students.join('\n')}`);
-  } catch (error) {
-    res.send(`${msg}${error.message}`);
-  }
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/plain');
+	res.send('Hello Holberton School!');
+	const msg = 'This is the list of our students\n';
+	try {
+		const students = await countStudents(DATABASE);
+		res.send(`${msg}${students.join('\n')}`);
+	} catch (error) {
+		res.send(`${msg}${error.message}`);
+	}
 });
 
 app.listen(port, () => { });
