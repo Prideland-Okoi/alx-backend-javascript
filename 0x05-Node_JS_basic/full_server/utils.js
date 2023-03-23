8da64773f0f62409ee3bb3d200abbf1c5015ab7b
Prideland-Okoi <prideland.okoi@gmail.com>
2023-03-23 01:23:46 +0000

    lint issue fixed

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,31 +1,32 @@
 const fs = require('fs');
 
-async function countStudents(path) {
-  let data;
-  try {
-    data = await fs.promises.readFile(path, 'utf8');
-  } catch (error) {
-    throw new Error('Cannot load the database');
-  }
-  const students = data.split('\r\n').slice(1)
-    .map((student) => student.split(','))
-    .map((student) => ({
-      firstName: student[0],
-      lastName: student[1],
-      age: student[2],
-      field: student[3],
-    }));
-  const fields = students.map((student) => student.field);
-  const uniqueFields = new Set(fields);
-  const studentsByField = {};
-  for (const field of uniqueFields) {
-    studentsByField[field] = [];
-  }
-  for (const student of students) {
-    studentsByField[student.field].push(student.firstName);
-  }
-  console.log(studentsByField);
-  return studentsByField;
+function readDatabase(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(Error(err));
+        return;
+      }
+      const content = data.toString().split('\n');
+
+      let students = content.filter((item) => item);
+
+      students = students.map((item) => item.split(','));
+
+      const fields = {};
+      for (const i in students) {
+        if (i !== 0) {
+          if (!fields[students[i][3]]) fields[students[i][3]] = [];
+
+          fields[students[i][3]].push(students[i][0]);
+        }
+      }
+
+      delete fields.field;
+
+      resolve(fields);
+    });
+  });
 }
 
-module.exports = countStudents;
+export default readDatabase;
