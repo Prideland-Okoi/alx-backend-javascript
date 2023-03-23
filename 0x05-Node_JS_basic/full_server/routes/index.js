03ef57035af29d6bc78b6bd6d9162aecf195f32f
Prideland-Okoi <prideland.okoi@gmail.com>
2023-03-23 01:25:52 +0000

    lint issue fixed

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -1,13 +1,22 @@
+import express from 'express';
 import AppController from '../controllers/AppController';
 import StudentsController from '../controllers/StudentsController';
 
-const express = require('express');
+function controllerRouting(app) {
+  const router = express.Router();
+  app.use('/', router);
 
-const router = express.Router();
+  router.get('/', (req, res) => {
+    AppController.getHomepage(req, res);
+  });
 
-router.get('/', (req, res) => AppController.getHomepage(req, res));
-router.get('/students', (req, res) => StudentsController.getAllStudents(req, res));
-router.get('/students/:major', (req, res) => StudentsController.getAllStudentsByMajor(req, res));
+  router.get('/students', (req, res) => {
+    StudentsController.getAllStudents(req, res, process.argv[2]);
+  });
 
-export default router;
-module.exports = router;
+  router.get('/students/:major', (req, res) => {
+    StudentsController.getAllStudentsByMajor(req, res, process.argv[2]);
+  });
+}
+
+export default controllerRouting;
